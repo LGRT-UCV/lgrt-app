ad4729cd1199c43de1496c3c1e715578d7ee8cb8
plip123 <cp.carlos.pino@gmail.com>
2024-07-19 22:06:03 -0400

    Fix redirects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,21 @@
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { Routes } from "./lib/constants";
+import { useLabProvider } from "@/context/labProvider";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
+  const { status } = useSession();
+  const { isConnected } = useLabProvider();
   const router = useRouter();
 
   useEffect(() => {
-    void router.push(Routes.Login);
-  }, []);
+    if (status === "authenticated" && isConnected) {
+      void router.push(Routes.Inventory);
+    } else {
+      void router.push(Routes.Login);
+    }
+  }, [status, isConnected]);
 
   return <div></div>;
 }

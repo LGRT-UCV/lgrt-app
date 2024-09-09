a2b34130d151c8903ed4bb370672d4c7fb2b1b48
plip123 <cp.carlos.pino@gmail.com>
2024-09-08 20:41:00 -0400

    Include task description

--- src/app/(laboratory)/projects/project/components/projectTasks.tsx
+++ src/app/(laboratory)/projects/project/components/projectTasks.tsx
@@ -1,5 +1,6 @@
-import { Button, Form, Input } from "antd";
-import { PlusOutlined, MinusCircleOutlined } from "@ant-design/icons";
+import { Button, Card, Form, Input } from "antd";
+import { PlusOutlined, CloseOutlined } from "@ant-design/icons";
+import TextArea from "antd/es/input/TextArea";
 
 export default function ProjectTasks() {
   return (
@@ -7,29 +8,57 @@ export default function ProjectTasks() {
       {(fields, { add, remove }) => (
         <>
           {fields.map(({ key, name, ...restField }) => (
-            <div key={key} className="mb-4 flex gap-8 pr-4">
-              <Form.Item
-                {...restField}
-                label="Nombre de la tarea"
-                name={[name, "name"]}
-                className="w-full"
-                rules={[
-                  {
-                    type: "string",
-                    required: true,
-                    max: 120,
-                    message: "Por favor verifique el nombre de la tarea",
-                  },
-                ]}
-              >
-                <Input placeholder="Nombre de la tarea" maxLength={120} />
-              </Form.Item>
-              <MinusCircleOutlined
-                onClick={() => {
-                  remove(name);
-                }}
-              />
-            </div>
+            <Card
+              size="small"
+              title={`Tarea #${name + 1}`}
+              key={key}
+              extra={
+                <CloseOutlined
+                  onClick={() => {
+                    remove(name);
+                  }}
+                />
+              }
+            >
+              <div className="w-full">
+                <Form.Item
+                  {...restField}
+                  label="Nombre de la tarea"
+                  name={[name, "name"]}
+                  className="w-full"
+                  rules={[
+                    {
+                      type: "string",
+                      required: true,
+                      max: 120,
+                      message: "Por favor verifique el nombre de la tarea",
+                    },
+                  ]}
+                >
+                  <Input placeholder="Nombre de la tarea" maxLength={120} />
+                </Form.Item>
+
+                <Form.Item
+                  label="Descripción"
+                  name="description"
+                  className="px-2"
+                  rules={[
+                    {
+                      type: "string",
+                      required: true,
+                      max: 500,
+                      message: "Por favor verifique la descripción de la tarea",
+                    },
+                  ]}
+                >
+                  <TextArea
+                    placeholder="Descripción de la tarea..."
+                    rows={4}
+                    maxLength={500}
+                  />
+                </Form.Item>
+              </div>
+            </Card>
           ))}
           <Form.Item>
             <Button
